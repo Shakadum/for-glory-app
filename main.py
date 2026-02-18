9820591d08ac9577b4edf4d51c0d25e20b09edbe
Shakadum <davidcarpy96@gmail.com>
2026-02-17 23:04:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -555,3 +555,6 @@ function addEmoji(e){if(currentEmojiTarget){let i=document.getElementById(curren
 </script>
 </body>
 </html>
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=10000)
+
