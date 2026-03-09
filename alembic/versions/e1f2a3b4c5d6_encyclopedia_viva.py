586935149772a4e16008ad9c58287408b525cf5a
David Ribeiro <davidcarpy96@gmail.com>
2026-03-09 15:39:55 -0300

    fixs correçoes

--- alembic/versions/e1f2a3b4c5d6_encyclopedia_viva.py
+++ alembic/versions/e1f2a3b4c5d6_encyclopedia_viva.py
@@ -8,7 +8,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision = 'e1f2a3b4c5d6'
-down_revision = 'a1b2c3d4e5f6'
+down_revision = 'b2c3d4e5f6a7'
 branch_labels = None
 depends_on = None
 
